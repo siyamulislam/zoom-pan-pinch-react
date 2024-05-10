479e59c1df038cdd0810ac5f52a247516ffe10e8
siyamulislam <siyamulislam1@gmail.com>
2024-05-10 19:15:19 +0600

    basic responsive

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import "./App.css";
+
+import { TransformWrapper, TransformComponent, useControls } from "react-zoom-pan-pinch";
+ 
+
+export default function App() { 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div className="App">
+      <TransformWrapper
+        minScale={0.01}
+        maxScale={10}
+        initialScale={1}
+        centerOnInit={true}
+      centerZoomedOut={true}
+      >
+        <Controls />
+        <div className="fullScreenImage">
+
+          <TransformComponent wrapperClass="wrapper" contentClass="content">
+            <img
+              className="tae"
+              height="100%"
+              width={"100%"}
+              src="https://res.cloudinary.com/siyam/image/upload/w_450/v1714969978/portfolio/images/trndvbj0piqjmueg75vi.jpg"
+              alt="test 2"
+            />
+
+          </TransformComponent>
+        </div>
+      </TransformWrapper>
     </div>
   );
 }
-
-export default App;
+const Controls = () => {
+  const { zoomIn, zoomOut, resetTransform } = useControls();
+  return (
+    <div  className="tool">
+      <button onClick={() => zoomIn()}>+</button>
+      <button onClick={() => zoomOut()}>-</button>
+      <button onClick={() => resetTransform()}>Reset</button>
+    </div>
+  );
+};
